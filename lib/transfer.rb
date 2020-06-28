e52f1a37ea65e4bee080dd373e191244624d7ad4
Flora Petersen <floramarina@comcast.net>
2020-06-28 03:24:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "rejected" || @sender.balance < @amount
+      @sender.balance < @amount
       "Transaction rejected. Please check your account balance."
     end
   end
