56865d713fc3d2ab48a10930c36a7a0657cd1cae
Flora Petersen <floramarina@comcast.net>
2020-06-28 03:33:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,9 +28,10 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status == "complete" 
+    if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
+      @status = "reversed"
     end
   end
 end
